537d7cecf8886328da8aa77fb7229102c8e107b8
Adnan9750 <adnansarfaraz975@gmail.com>
2024-01-27 01:36:26 +0500

    signup route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,18 +1,26 @@
 import userModel from "../models/userModel.js";
+import bcrypt from 'bcrypt'
 
 export const SignUp = async (req,res)=>{
-    const {username,email,password} = req.body;
-    const userExist = await userModel.findOne({email:email})
+    try {
+        const {username,email,password} = req.body;
+        // const userExist = await userModel.findOne({email:email})
 
-    if(userExist) {
-        res.json("Email Already Exists")
+        // if(userExist) {
+        //     res.json({message:"Email Already Exists"})
+        // }
+
+        if(!username || !email || !password || username === '' || email === '' || password === ''){
+            res.status(400).json({message: "All fields are required"});
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10)
+
+        const newUser = new userModel({username,email,password:hashedPassword}); 
+        await newUser.save();
+        res.json("SignUp Successfully")    
+    } catch (error) {
+        res.status(500).json({message:error.message})
     }
-
-    if(!username || !email || !password || username === '' || email === '' || password === ''){
-        res.status(400).json({message: "All fields are required"});
-    }
-
-    const newUser = new userModel({username,email,password}); 
-    await newUser.save();
-    res.json("SignUp Successfully")
+    
 }
