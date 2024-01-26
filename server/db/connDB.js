5ed5818450d77a6b680f9617ed7df2553007c513
Adnan9750 <adnansarfaraz975@gmail.com>
2024-01-26 22:56:30 +0500

    database connection complete

--- server/db/connDB.js
+++ server/db/connDB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connDB = async () =>{
     try {
-        await mongoose.connect('mongodb://adnansarfaraz:Adnan975@ac-1tpvn8w-shard-00-00.xbhdblq.mongodb.net:27017,ac-1tpvn8w-shard-00-01.xbhdblq.mongodb.net:27017,ac-1tpvn8w-shard-00-02.xbhdblq.mongodb.net:27017/Blog-App?ssl=true&replicaSet=atlas-2q3f10-shard-0&authSource=admin&retryWrites=true&w=majority')
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("Successfully connected");
     } catch (error) {
         console.log(error);
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
 import { connDB } from './db/connDB.js';
 const app = express()
+const port = process.env.PORT 
 
 connDB();
 
 
-app.listen(8000,()=>{
-    console.log("Server is running at port 8000");
+app.listen(port,()=>{
+    console.log(`Server is running at port ${port}`);
 })
