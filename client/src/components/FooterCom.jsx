b87c5b6e8cd08abc9439bbee4f288b1bb8ed5760
Adnan9750 <adnansarfaraz975@gmail.com>
2024-01-30 02:26:04 +0500

    complete footer

--- client/src/components/FooterCom.jsx
+++ client/src/components/FooterCom.jsx
@@ -1,15 +1,16 @@
 import { Footer } from 'flowbite-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {BsFacebook, BsGithub, BsInstagram, BsLinkedin} from 'react-icons/bs'
 
 const FooterCom = () => {
   return (
     <>
       <Footer container className='border border-t-8 border-teal-700'>
-        <div>
-            <div>
+        <div className='w-full max-w-7xl mx-auto'>
+            <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
                 {/* link div */}
-                <div>
+                <div className='mt-5'>
                     <Link to='/' className='self-center whitespace-nowrap font-semibold text-sm sm:text-xl dark:text-white'>
                         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500
                                 rounded-md text-white'>
@@ -18,7 +19,57 @@ const FooterCom = () => {
                         Blog
                     </Link>
                 </div>
-                <div></div>
+                <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+                    <div>
+                        <Footer.Title title='About' />
+                        <Footer.LinkGroup col>
+                            <Footer.Link
+                             href='https://github.com/Adnan9750'
+                             target='_blank'>
+                                My Project
+                            </Footer.Link>
+                            <Footer.Link
+                             href='/about'
+                             target='_blank'>
+                                Adnan's Blog
+                            </Footer.Link>
+                        </Footer.LinkGroup>
+                    </div>
+                    <div>
+                        <Footer.Title title='Follow Us' />
+                        <Footer.LinkGroup col>
+                            <Footer.Link
+                             href='https://github.com/Adnan9750'
+                             target='_blank'>
+                                Github
+                            </Footer.Link>
+                            <Footer.Link href='#'>
+                                Adnan's Blog
+                            </Footer.Link>
+                        </Footer.LinkGroup>
+                    </div>
+                    <div>
+                        <Footer.Title title='Legal' />
+                        <Footer.LinkGroup col>
+                            <Footer.Link href='#'>
+                                Privacy Policy
+                            </Footer.Link>
+                            <Footer.Link href='#'>
+                                Terms &amp; Condition
+                            </Footer.Link>
+                        </Footer.LinkGroup>
+                    </div>
+                </div>
+            </div>
+            <Footer.Divider />
+            <div className='w-full sm:flex  sm:justify-between'>
+                <Footer.Copyright by="Adnan's Blog" year={new Date().getFullYear()} />
+                <div className='flex gap-5 mt-4 sm:mt-0 sm:justify-center'>
+                    <Footer.Icon href='#' icon={BsFacebook}/>
+                    <Footer.Icon href='#' icon={BsInstagram}/>
+                    <Footer.Icon href='https://github.com/Adnan9750' icon={BsGithub}/>
+                    <Footer.Icon href='#' icon={BsLinkedin}/>
+                </div>
             </div>
         </div>
       </Footer>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -44,22 +44,22 @@ const Header = () => {
         </div>
         {/* Navbar Menu */}
         <Navbar.Collapse>
-                <Navbar.Link active={path === '/'} as={'div'}>
-                    <Link to='/'>
-                        Home
-                    </Link>
-                </Navbar.Link>
-                <Navbar.Link active={path === '/about'} as={'div'}>
-                    <Link to='/about'>
-                        About
-                    </Link>
-                </Navbar.Link>
-                <Navbar.Link active={path === '/project'} as={'div'}>
-                    <Link to='/projects'>
-                        Projects
-                    </Link>
-                </Navbar.Link>
-            </Navbar.Collapse>
+            <Navbar.Link active={path === '/'} as={'div'}>
+                <Link to='/'>
+                    Home
+                </Link>
+            </Navbar.Link>
+            <Navbar.Link active={path === '/about'} as={'div'}>
+                <Link to='/about'>
+                    About
+                </Link>
+            </Navbar.Link>
+            <Navbar.Link active={path === '/projects'} as={'div'}>
+                <Link to='/projects'>
+                    Projects
+                </Link>
+            </Navbar.Link>
+        </Navbar.Collapse>
       </Navbar>
     </>
   )
