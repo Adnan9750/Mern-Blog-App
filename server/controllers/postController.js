ebff6f2c2bc5c77085c685937c64b26ef166909d
Adnan9750 <adnansarfaraz975@gmail.com>
2024-02-26 20:51:52 +0500

    complete getPost api route

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -18,3 +18,51 @@ export const createPost = async (req, res, next) => {
     const savedPost = await newPost.save()
     res.status(201).json(savedPost)
 }
+
+export const getPosts = async (req,res,next) => {
+    try {
+        // this start is for pagination mean if in query if there is some specific value then start from that
+        // otherwise it always start from zero
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const orderDirection = req.query.order === 'asc' ? 1 : -1 
+        const posts = await PostModel.find({
+            ...(req.query.userID && {userID: req.query.userID}),
+            ...(req.query.category && {category: req.query.category}),
+            ...(req.query.slug && {slug:req.query.slug}),
+            ...(req.query.postId && {_id:req.query.postId}),
+            ...(req.query.searchTerm && {
+                // this $or is for that it check the search term data in both title and content of post
+                // and then give that data
+                $or : [
+                    // this option 'i' is for either upper or lower case this not matter
+                    { title: {$regex: req.query.searchTerm, $options: 'i'} },
+                    { content: {$regex: req.query.searchTerm, $options: 'i'} }
+                ],
+            }),
+        }).sort({updatedAt:orderDirection}).skip(startIndex).limit(limit)
+
+        const totalPosts = await PostModel.countDocuments()
+
+        const nowDate = new Date();
+
+        const oneMonthAgo = new Date(
+            nowDate.getFullYear(),
+            nowDate.getMonth() - 1,
+            nowDate.getDate()
+        );
+
+        const lastMonthPosts = await PostModel.countDocuments({
+            createdAt: {$gte: oneMonthAgo}
+        })
+
+        res.status(200).json({
+            posts,
+            totalPosts,
+            lastMonthPosts
+        })
+
+    } catch (error) {
+        next(error)
+    }
+}
--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -1,10 +1,11 @@
 
 import express from 'express'
 import { userVerification } from '../utils/userVerification.js';
-import { createPost } from '../controllers/postController.js';
+import { createPost, getPosts } from '../controllers/postController.js';
 
 const router = express.Router();
 
 router.post('/create',userVerification,createPost)
+router.get('/getposts',getPosts)
 
 export default router
