8e79ad2db60404ce856f51addb50d40e0aac2e2d
Adnan Sarfaraz <adnansarfaraz975@gmail.com>
2024-05-06 06:19:41 -0700

    Show comments data

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import moment from 'moment'
 
 const Comment = ({currentComment}) => {
 
@@ -23,9 +24,20 @@ const Comment = ({currentComment}) => {
 
   return (
     <>
-      <div>
+    <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
+      <div className='flex-shrink-0 mr-3'>
         <img src={user.avatar} alt={user.username} className='w-10 h-10 rounded-full bg-gray-200' />
       </div>
+      <div className='flex-1'>
+        <div className='flex items-center mb-1'>
+          <span className='font-bold mr-1 text-xs truncate'>
+            {user ? `@${user.username}`: 'anonymous user'}
+          </span>
+          <span className='text-gray-500 text-xs'>{moment(currentComment.createdAt).fromNow()}</span>
+        </div>
+        <p className='text-gray-500 pb-2'>{currentComment.commentContent}</p>
+      </div>
+    </div>
     </>
   )
 }
